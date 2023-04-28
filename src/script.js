4decaca2e8c71266924239e4b088f9445fc4e0cd
Breno Teixeira <brenotm10@gmail.com>
2023-04-27 23:44:25 -0300

    ajustes

--- src/script.js
+++ src/script.js
@@ -1,18 +1,20 @@
-var nome = (prompt("Qual Seu Nome?"))
-
-alert("Bem vindo a Loja Leftei, " + nome)
 
 
-let t = document.getElementById("section-titulo")
+function section(){
 
-t.addEventListener("mouseover", mouseOver)
-t.addEventListener("mouseout", mouseout)
-
-function mouseOver(){
-    this.innerHTML = "Também fazemos peças à gosto do cliente"
-    this.style.backgroundColor = "red"
+    let t = document.getElementById("section-titulo")
+    
+    t.addEventListener("mouseover", mouseOver)
+    t.addEventListener("mouseout", mouseout)
+    
+    function mouseOver(){
+        this.innerHTML = "Também fazemos peças à gosto do cliente"
+        this.style.backgroundColor = "red"
+    }
+    function mouseout(){
+        this.innerHTML = "Algumas variedades de peças que você encontra aqui na Leftei"
+        this.style.backgroundColor = "black"
+    }
 }
-function mouseout(){
-    this.innerHTML = "Algumas variedades de peças que você encontra aqui na Leftei"
-    this.style.backgroundColor = "black"
-}
+
+section();
