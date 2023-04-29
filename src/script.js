830cabde99351ff687c30cafbe4bc58d2ae7d7fb
Breno Teixeira <brenotm10@gmail.com>
2023-04-28 22:19:18 -0300

    adicionando responsividade e carrossel

--- src/script.js
+++ src/script.js
@@ -18,3 +18,47 @@ function section(){
 }
 
 section();
+
+function carrossel(){
+
+    const botoes = document.querySelectorAll('.botao');
+    const imgs = document.querySelectorAll('.img');
+    
+    
+    botoes.forEach((botao, i) => {
+        botao.addEventListener('click', function(){
+            desativarBotao();
+    
+            marcarBotaoAtivo(botao);
+   
+            esconderImagemAtiva();
+
+            mostrarImagem(i);
+        });
+    });
+
+    function desativarBotao(){
+        const botaoRemov = document.querySelector('.on');
+        botaoRemov.classList.remove('on');
+    };
+    
+    function marcarBotaoAtivo(botao){
+        botao.classList.add('on');
+    };
+    
+    function esconderImagemAtiva(){
+        const esconderImg = document.querySelector('.ativo');
+        esconderImg.classList.remove('ativo');
+    };
+    
+    function mostrarImagem(i){
+        imgs[i].classList.add('ativo');
+    };
+};
+
+carrossel();
+    
+    
+    
+    
+    
